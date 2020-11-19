5556659c52e3ac47d6f5002fcac34bcff167f7c0
anon <mhyzy155@gmail.com>
2020-11-19 14:57:18 +0100

    added finding and decoding the markers

--- scripts/explore_area_functions.py
+++ scripts/explore_area_functions.py
@@ -13,12 +13,17 @@ while not rospy.is_shutdown():
   g_range_ahead = functions.distance()
   print g_range_ahead
 
-  if g_range_ahead < 0.8:
-    functions.move(0.0, 0.4)
-    print "Turn"
+  if functions.marker_is_visible():
+    functions.stop()
+    print "Found the marker!"
+    print(functions.marker_decode())
   else:
-    functions.move(0.4, 0.0)
-    print "Drive"
+    if g_range_ahead < 0.8:
+      functions.move(0.0, 0.2)
+      print "Turn"
+    else:
+      functions.move(0.4, 0.0)
+      print "Drive"
   rate.sleep()
 
 # END ALL
--- scripts/functions.py
+++ scripts/functions.py
@@ -1,10 +1,14 @@
 import rospy
 from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import Twist
+from std_msgs.msg import String
 
 vel_pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
 scan_sub = [None]
 dist = [None]
+marker_msg_sub = [None]
+marker_msg = [None]
+marker_pos_sub = [None]
 
 def scan_callback(msg, arg):
     tmp=[msg.ranges[0]]
@@ -14,15 +18,24 @@ def scan_callback(msg, arg):
         tmp.append(msg.ranges[i])
     arg[0] = min(tmp)
 
+def marker_msg_callback(msg, arg):
+    arg[0] = msg.data
+
 def distance():
+    """
+    Returns the distance to the closest object.
+    """
     if scan_sub[0] == None:
         scan_sub[0] = rospy.Subscriber('scan', LaserScan, scan_callback, dist)
-    while dist[0]==None:
+    while dist[0] == None:
         rospy.sleep(1)
     #sub.unregister()
     return dist[0]
 
 def move(vel_forward, vel_angular):
+    """
+    move(vel_forward, vel_angular)
+    """
     twist = Twist()
     twist.linear.x = vel_forward
     twist.angular.z = vel_angular
@@ -30,9 +43,35 @@ def move(vel_forward, vel_angular):
     #pub.unregister()
 
 def stop():
+    """
+    Stops the robot.
+    """
     vel_pub.publish(Twist())
     #pub.unregister()
 
-def decode():
-    pass
-    #return [(x,y), (x_next,y_next), nr, letter]
+def marker_is_visible():
+    """
+    Returns True if the marker is visible, otherwise False.
+    """
+    marker_msg[0] = None
+    if marker_msg_sub[0] == None:
+        marker_msg_sub[0] = rospy.Subscriber('/visp_auto_tracker/code_message', String, marker_msg_callback, marker_msg)
+    while marker_msg[0] == None:
+        rospy.sleep(1)
+    if marker_msg[0] == "":
+        return False
+    else:
+        return True
+
+def marker_decode():
+    """
+    Returns [(x,y), (x_next,y_next), marker_no, letter]
+    """
+    if marker_msg_sub[0] == None:
+        marker_msg_sub[0] = rospy.Subscriber('/visp_auto_tracker/code_message', String, marker_msg_callback, marker_msg)
+    while marker_msg[0] == None:
+        rospy.sleep(1)
+    tmp = marker_msg[0].split("\r\n")
+    for i, word in enumerate(tmp):
+        tmp[i] = word.split("=")[1]
+    return [(float(tmp[0]),float(tmp[1])), (float(tmp[2]),float(tmp[3])), int(tmp[4]), tmp[5]]
