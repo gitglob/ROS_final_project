9cce74f430c46040f18a5da4c082645010fa0bdc
KarolKubala <kkubala.karol@gmail.com>
2020-11-29 16:54:54 +0100

    new add

--- scripts/my_function.py
+++ scripts/my_function.py
@@ -1,18 +1,35 @@
+def rotate():
+# Setting the current time for distance calculus
+t0 = rospy.Time.now().to_sec()
+current_angle = 0
+relative_angle = 360
+	while(current_angle < relative_angle):
+		if not functions.marker_is_visible():
+		# Rotating 360 degrees
+			functions.adjust(0.0, -(360*3.14/360))
+			t1 = rospy.Time.now().to_sec()
+			current_angle = -(360*3.14/360)*(t1-t0)
+		else:
+			current_angle = relative_angle
+
+
+
 def go_to_marker(markers_pose_map):
 	if markers_letter.count(None) == 2:
 		markers_pose_map[mark_no].position.x - 7
 		markers_pose_map[mark_no].position.y - 7
 		functions.movebase_goal_execute(markers_pose_map[mark_no])
-		# Setting the current time for distance calculus
-		t0 = rospy.Time.now().to_sec()
-		current_angle = 0
-		relative_angle = 360
-		while(current_angle < relative_angle):
-			if not functions.marker_is_visible():
-		# Rotating 360 degrees
-				functions.adjust(0.0, -(360*3.14/360))
-				t1 = rospy.Time.now().to_sec()
-				current_angle = angular_speed*(t1-t0)
+
+		t_0 = rospy.Time.now().to_sec()
+		distance_to_travel = 3
+		current_distance = 0
+		for(current_distance < distance_to_travel):
+			if not functions.marker_is_visible()):
+				t_1 = rospy.Time.now().to_sec()
+				current_distance = 0.4*(t_1 - t_0)
+				functions.move(0.4, 0.0)
+				rotate()
 			else:
-				current_angle = relative_angle
+				current_distance = distance_to_travel
+
 
